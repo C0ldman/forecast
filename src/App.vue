9cee9699dee629abe09c58f358fc4c9de1153fab
Y.Ukrainets <yuriy.ukrainets@gmail.com>
2024-11-13 14:05:58 +0200

    Add logo

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
       <nav>
         <RouterLink to="/">Main</RouterLink>
         <RouterLink to="/favourites">Favourites</RouterLink>
